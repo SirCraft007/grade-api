35be100d6bd021a7e6f311ca87f30c866714c28e
SirCraft007 <seramol@gmx.ch>
2024-06-13 11:17:25 +0200

    readded app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from api import api_routes
+from datetime import datetime
+from flask_sitemapper import Sitemapper
+from keygen import generate_api_key
+import os
+
+app = Flask(__name__, static_folder="static", static_url_path="")
+
+sitemapper = Sitemapper()
+sitemapper.init_app(app)
+
+app.register_blueprint(api_routes)
+
+app.config["SECRET_KEY"] = generate_api_key()
+
+
+@app.context_processor
+def inject_current_year():
+    return dict(current_year=datetime.now().year)
+
+
+@sitemapper.include(
+    lastmod=datetime.now().strftime("%Y-%m-%d"),
+    changefreq="daily",
+    priority=1.0,
+)
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@sitemapper.include(
+    lastmod=datetime.now().strftime("%Y-%m-%d"), changefreq="daily", priority=0.8
+)
+@app.route("/docs")
+@app.route("/documentation")
+def docs():
+    return render_template("docs.html")
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return sitemapper.generate()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
